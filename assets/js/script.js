e4350e7b9c70e76ae33282abe34802d1da67932b
Mark Pavel <markpavel02@gmail.com>
2022-04-19 19:22:46 -0400

    Added Highscore Viewing And Fixed Progression Bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var main = document.querySelector("main")
+var viewScores = document.querySelector("a")
 var timerEl = document.querySelector("p[id='timer']")
 var startingEls = document.querySelector("section[id='start']")
 var currentQuestion
@@ -39,8 +40,13 @@ function buttonHandler(event) {
     else if (target.matches(".answer")) {    
         answerHandler()
     }
-    else if (target.matches(".go-back")) {
+    else if (target.matches("#go-back")) {
         goBackHandler()
+    }
+    else if (target.matches("#return")) {
+        main.removeChild(currentQuestion)
+        currentQuestion = null
+        main.appendChild(startingEls)
     }
 
     function startButtonHandler() {
@@ -49,7 +55,7 @@ function buttonHandler(event) {
         currentQuestion = createQuestion()
         main.appendChild(currentQuestion)
     
-        timerEl.textContent = "Time: 45"
+        setTime(45)
     
         quiz.time = 45
         quiz.timer = setInterval(timerCountDown,1000)
@@ -58,7 +64,7 @@ function buttonHandler(event) {
         function timerCountDown() {
             quiz.time--
         
-            timerEl.textContent = "Time: " + quiz.time
+            setTime(quiz.time)
         
             if (quiz.time <= 0) {
                 clearInterval(quiz.timer)
@@ -66,6 +72,8 @@ function buttonHandler(event) {
                 main.removeChild(currentQuestion)
                 currentQuestion = createFinalTally()
                 main.appendChild(currentQuestion)
+
+                setTime(0)
             }
         }
     }
@@ -78,13 +86,18 @@ function buttonHandler(event) {
         else {
             currentQuestion.querySelector("p").textContent = "wrong"
             quiz.waiting = false
-            quiz.time = 0
-            return
+            quiz.time = quiz.time - 5
+
+            if (quiz.time <= 0) {
+                return
+            }
         }
     
         setTimeout(function() {
             if (quiz.questionCounter + 1 > questions.length) {
                 clearInterval(quiz.timer)
+
+                setTime(0)
 
                 main.removeChild(currentQuestion)
 
@@ -114,6 +127,7 @@ function buttonHandler(event) {
         saveScore(name)
     
         main.removeChild(currentQuestion)
+        currentQuestion = null
         main.appendChild(startingEls)
     
         quiz.waiting = false
@@ -177,13 +191,58 @@ function buttonHandler(event) {
     
         var goBack = document.createElement("button")
         goBack.textContent = "Go Back"
-        goBack.className = "btn go-back"
+        goBack.className = "btn"
+        goBack.id = "go-back"
         inputWrapper.appendChild(goBack)
     
         finalTally.appendChild(inputWrapper)
     
         return finalTally
     }
+}
+function viewScoresHandler(event) {
+    event.preventDefault()
+    
+    clearInterval(quiz.timer)
+    setTime(0)
+
+    if(currentQuestion) {
+        main.removeChild(currentQuestion)
+    }
+    else {
+        main.removeChild(startingEls)
+    }
+
+    loadScores()
+
+    currentQuestion = document.createElement("div")
+    if (!quiz.scores) {
+
+        var label = document.createElement("p")
+        label.textContent = "There are no scores."
+        currentQuestion.appendChild(label)
+    }
+    else {
+        quiz.scores.forEach(element => {
+            var container = document.createElement("div")
+            
+            var name = document.createElement("p")
+            name.textContent = "Name: " + element.savName
+            container.appendChild(name)
+
+            var score = document.createElement("p")
+            score.textContent = "Score: " + element.savScore
+            container.appendChild(score)
+
+            currentQuestion.appendChild(container)
+        });
+    }
+    var button = document.createElement("button")
+    button.textContent = "Go Back"
+    button.id = "return"
+    currentQuestion.appendChild(button)
+
+    main.appendChild(currentQuestion)
 }
 function saveScore(name) {
     loadScores()
@@ -204,5 +263,9 @@ function loadScores() {
 
     quiz.scores = JSON.parse(quiz.scores)
 }
+function setTime(time) {
+    timerEl.textContent = "Time: " + time
+}
 
 main.addEventListener("click", buttonHandler)
+viewScores.addEventListener("click", viewScoresHandler)
